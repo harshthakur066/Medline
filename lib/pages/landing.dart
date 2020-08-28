e82e4beda7b1daadfca0fd3743a5955356d8f12b
Hassan Shahid <hassanshahidofficial@gmail.com>
2020-08-28 14:31:32 +0530

    landing, signup, login page

---
+++ lib/pages/landing.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:medline/pages/login.dart';
+import 'package:medline/pages/signup.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final phoneHeight = MediaQuery.of(context).size.height;
+    final phoneWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        height: phoneHeight,
+        width: phoneWidth,
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 20),
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Welcome',
+                        style: TextStyle(
+                            fontSize: 75, fontWeight: FontWeight.bold)),
+                    //SizedBox(height: 20),
+                    Text('to your online medical consultation portal.',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18)),
+                  ]),
+              SizedBox(height: 200),
+              MaterialButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginScreen()));
+                },
+                child: Text('LOGIN',
+                    style: TextStyle(color: Colors.black, fontSize: 18)),
+                height: 50,
+                minWidth: double.infinity,
+                color: Color(0xFF08ffbd),
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)),
+              ),
+              SizedBox(height: 15),
+              MaterialButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignupScreen()));
+                },
+                child: Text('REGISTER',
+                    style: TextStyle(color: Colors.black, fontSize: 18)),
+                height: 50,
+                minWidth: double.infinity,
+                color: Color(0xFF08ffbd),
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/signup.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:medline/pages/login.dart';
+
+class SignupScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final phoneHeight = MediaQuery.of(context).size.height;
+    var person = Icons.person;
+    return Scaffold(
+      body: Container(
+        //height: phoneHeight,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Sign Up',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('Please enter your credentials'),
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.transparent,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      //obscureText: obscureText,
+                      decoration: InputDecoration(
+                        hintText: 'Enter Username',
+                        prefixIcon: Icon(Icons.person, color: Color(0xFF08ffbd)),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey[400],
+                          ),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                      //obscureText: obscureText,
+                      decoration: InputDecoration(
+                        hintText: 'Enter Mobile Number',
+                        prefixIcon: Icon(Icons.phone, color: Color(0xFF08ffbd)),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey[400],
+                          ),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                      //obscureText: obscureText,
+                      decoration: InputDecoration(
+                        hintText: 'Enter Email ID',
+                        prefixIcon: Icon(Icons.email, color: Color(0xFF08ffbd)),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey[400],
+                          ),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                      //obscureText: obscureText,
+                      decoration: InputDecoration(
+                        hintText: 'Enter Password',
+                        prefixIcon: Icon(Icons.vpn_key, color: Color(0xFF08ffbd)),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey[400],
+                          ),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    MaterialButton(
+                      onPressed: () {},
+                      child: Text('SIGN UP',
+                          style: TextStyle(color: Colors.white, fontSize: 18)),
+                      height: 50,
+                      minWidth: double.infinity,
+                      color: Color(0xFF08ffbd),
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)),
+                    ),
+                    SizedBox(height: 10),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context) =>LoginScreen()));
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Already Registered?'),
+                          SizedBox(width: 5),
+                          Text(
+                            'Login!',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
